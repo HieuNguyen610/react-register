b493c51f45ccdaef47de1be5dde78af51249e569
hieunm2 <hieunm2@vnext.vn>
2025-04-15 17:29:12 +0700

    init

---
+++ src/main/java/hieu/reactuserregister/ReactUserRegisterApplication.java
@@ -0,0 +1,13 @@
+package hieu.reactuserregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactUserRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactUserRegisterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hieu/reactuserregister/ReactUserRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package hieu.reactuserregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactUserRegisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
